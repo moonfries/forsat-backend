2b62c292afb6c333f37ee18e30061f67656c30ef
mac <mac@macs-MacBook-Pro.local>
2021-06-10 15:49:55 +0800

    laravel codes

---
+++ app/Models/Models/Lookups/Country.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Models\Lookups;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    public $timestamps = false;
+}
